999bea3b28af90d18391f8962c52ac9498a24299
Shehan <Shehan@SHEHANV>
2020-04-14 11:23:33 +0530

    created newAnswers action.

--- PersonalityIndicatorFinal/Controllers/HomeController.cs
+++ PersonalityIndicatorFinal/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace PersonalityIndicatorFinal.Controllers
 {
     public class HomeController : Controller
     {
-
+       
         public ActionResult Index()
         {
             var questionWithAnswers = new QuestionWithAnswers()
@@ -33,16 +33,21 @@ namespace PersonalityIndicatorFinal.Controllers
                         QuestionNumber = 2, Questions = "what is your name?",
                         Answers = new List<Answer>()
                         {
-                            new Answer() {AnswerNumber = 1, Answers = "12",},
-                            new Answer() {AnswerNumber = 2, Answers = "23",},
-                            new Answer() {AnswerNumber = 3, Answers = "28",},
-                            new Answer() {AnswerNumber = 4, Answers = "35",}
+                            new Answer() {AnswerNumber = 1, Answers = "hasith",},
+                            new Answer() {AnswerNumber = 2, Answers = "nimal",},
+                            new Answer() {AnswerNumber = 3, Answers = "kamal",},
+                            new Answer() {AnswerNumber = 4, Answers = "sunil",}
                         }
                     }
                 }
             };
 
             return View(questionWithAnswers);
+        }
+        [HttpPost]
+        public ActionResult AddAnswers(NewAnswers newAnswers)
+        {
+            return View();
         }
 
         public ActionResult About()
